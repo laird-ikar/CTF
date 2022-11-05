b5cc516e53514eaf470fef069684a28050bffac2
laird-ikar <bguyot@student.42mulhouse.fr>
2022-11-05 18:44:31 +0100

    change script to work on my machine

--- cryptography/rsa_hastad_broadcast_attack.py
+++ cryptography/rsa_hastad_broadcast_attack.py
@@ -1,8 +1,9 @@
-import gmpy
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+import gmpy2 as gmpy
 
-e = 
+e =  
 
-n1 = 
+n1 =
 n2 = 
 n3 = 
 
@@ -11,9 +12,9 @@ c2 =
 c3 = 
 
 N = n1*n2*n3
-N1 = N/n1
-N2 = N/n2
-N3 = N/n3
+N1 = n2*n3
+N2 = n1*n3
+N3 = n1*n2
 
 u1 = gmpy.invert(N1, n1)
 u2 = gmpy.invert(N2, n2)
@@ -21,6 +22,7 @@ u3 = gmpy.invert(N3, n3)
 
 M = (c1*u1*N1 + c2*u2*N2 + c3*u3*N3) % N
 
-m = gmpy.root(M,e)[0]
+m = gmpy.iroot(M,e)[0]
 
-print hex(m)[2:].rstrip("L").decode("hex")
+
+print (bytes.fromhex(hex(m)[2:].rstrip("L")).decode("ascii"))
